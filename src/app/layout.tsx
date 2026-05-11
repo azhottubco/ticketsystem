92868ca5263c9c553fe743dc220fa08e92046738
azhottubco <logan@azhottubco.com>
2026-05-11 12:00:08 -0700

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist } from 'next/font/google'
 import './globals.css'
 import { Toaster } from '@/components/ui/sonner'
 import { SessionProvider } from 'next-auth/react'
+import { ThemeProvider } from '@/components/theme/ThemeProvider'
 
 const geist = Geist({ subsets: ['latin'], variable: '--font-geist-sans' })
 
@@ -13,12 +14,14 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${geist.variable} font-sans antialiased`}>
-        <SessionProvider>
-          {children}
-        </SessionProvider>
-        <Toaster richColors position="top-right" />
+        <ThemeProvider>
+          <SessionProvider>
+            {children}
+          </SessionProvider>
+          <Toaster richColors position="top-right" />
+        </ThemeProvider>
       </body>
     </html>
   )
